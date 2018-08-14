7bbf84d3a1dee35eb231f4a459aa6f2cc6e7c811
Tobias Urdin <tobias.urdin@binero.se>
2018-08-14 11:55:38 +0200

    Add metadata-json-lint to rake_tasks
    Includes the rake tasks for metadata-json-lint
    so we can use it with rake.
    
    The metadata-json-lint is already a dependency
    for puppet-openstack_spec_helper.
    
    Change-Id: I527238f717c21d709f533366ac4af861c89484b8

--- lib/puppet-openstack_spec_helper/rake_tasks.rb
+++ lib/puppet-openstack_spec_helper/rake_tasks.rb
@@ -2,6 +2,7 @@
 require 'puppetlabs_spec_helper/rake_tasks'
 require 'puppet-lint/tasks/puppet-lint'
 require 'puppet-syntax/tasks/puppet-syntax'
+require 'metadata-json-lint/rake_task'
 require 'json'
 
 modname = JSON.parse(open('metadata.json').read)['name'].split('-')[1]
