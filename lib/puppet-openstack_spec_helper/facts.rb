c71da351fb834e17bfdeac0fcb2ec001f91c5511
Takashi Kajinami <tkajinam@redhat.com>
2022-07-22 14:05:22 +0900

    Stop setting @default_fact
    All unit test implementations have been updated and @default_fact is no
    longer directly used.
    
    Change-Id: Ic3fcada2601a7c9ba4089e2e8f1978768f1b6f08

--- lib/puppet-openstack_spec_helper/facts.rb
+++ lib/puppet-openstack_spec_helper/facts.rb
@@ -4,12 +4,6 @@ require 'puppet-openstack_spec_helper/defaults'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
-  # TODO(aschultz): remove this after all tests are converted to OSDefaults
-  # instead of referenceing @defaults_facts directly
-  c.before :each do
-    @default_facts = OSDefaults.get_facts
-  end
-
   # add custom facts globally for anyone using rspec-puppet-facts
   add_custom_fact :os_service_default, '<SERVICE DEFAULT>'
   add_custom_fact :os_workers, '2'
