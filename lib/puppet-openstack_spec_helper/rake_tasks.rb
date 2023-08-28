56cf30b7e26d2c1a440a83f67ec4d7041dcf5cdb
Takashi Kajinami <tkajinam@redhat.com>
2023-08-28 09:48:17 +0900

    Disable check_unsafe_interpolations
    The latest release puppet-lint contained the fix in the logic to parse
    interpolations in strings, which triggered the following lint warnings
    in multiple modules.
    
    WARNING: unsafe interpolation of variable 'foo' in exec command
    
    This disables the lint check as a short-term fix in master as well as
    stable branches. The check will be restored once the error is addressed
    in master.
    
    Change-Id: I7c4fea1d1f5ca1132f922d560c04daa4ac50e3db

--- lib/puppet-openstack_spec_helper/rake_tasks.rb
+++ lib/puppet-openstack_spec_helper/rake_tasks.rb
@@ -19,7 +19,12 @@ PuppetLint::RakeTask.new :lint do |config|
   config.ignore_paths = ["spec/**/*.pp", "vendor/**/*.pp"]
   config.fail_on_warnings = true
   config.log_format = '%{path}:%{line}:%{KIND}: %{message}'
-  config.disable_checks = ["80chars", "class_inherits_from_params_class", "only_variable_string"]
+  config.disable_checks = [
+    "80chars",
+    "class_inherits_from_params_class",
+    "only_variable_string",
+    "check_unsafe_interpolations"
+  ]
 end
 
 desc "Run acceptance tests"
