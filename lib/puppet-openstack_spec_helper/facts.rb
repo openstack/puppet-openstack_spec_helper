7d7226379dd4612b3586dc1ab32934f2a6f9fa60
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-13 23:59:13 +0900

    Inject worker facts as integers
    ... to simulate the type in actual deployments correctly.
    
    Change-Id: Ie5ccaeceb2a1176115620ea486ccd5d9010dc0ac
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- lib/puppet-openstack_spec_helper/facts.rb
+++ lib/puppet-openstack_spec_helper/facts.rb
@@ -6,7 +6,7 @@ include RspecPuppetFacts
 RSpec.configure do |c|
   # add custom facts globally for anyone using rspec-puppet-facts
   add_custom_fact :os_service_default, '<SERVICE DEFAULT>'
-  add_custom_fact :os_workers, '2'
-  add_custom_fact :os_workers_heat_engine, '2'
-  add_custom_fact :os_workers_keystone, '4'
+  add_custom_fact :os_workers, 2
+  add_custom_fact :os_workers_heat_engine, 2
+  add_custom_fact :os_workers_keystone, 4
 end
